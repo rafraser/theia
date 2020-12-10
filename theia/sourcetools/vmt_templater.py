e19595aa30e9f4594e93f08cf7f1eb42c3f5ed0f
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-12-11 00:22:53 +1000

    Start work on some VTF/VMT conversion stuff

---
+++ theia/sourcetools/vmt_templater.py
@@ -0,0 +1,42 @@
+def convert_to_vtf(infile: str, format: str = "dxt5", pause: bool = False):
+    """Use VTFCmd to convert a single input image into a VTF
+    VTFCmd.exe must exist at the root level for this to work
+
+    Args:
+        infile (str): Input image file
+        format (str, optional): VTF format mode. Defaults to "dxt5".
+        pause (bool, optional): Whether to wait for conversion before continuing. Defaults to False.
+    """
+    args = ["./VTFCmd.exe", "-file", infile, "-format", format, "-silent"]
+    sp = subprocess.Popen(args)
+    if pause:
+        sp.wait()
+
+
+def convert_folder_to_vtf(
+    indir: str, outdir: str, format: str = "dxt5", pause: bool = False
+):
+    """Use VTFCmd to convert a single input image into a VTF
+    VTFCmd.exe must exist at the root level for this to work
+
+    Args:
+        indir (str): Input directory. This will only convert .png images.
+        outdir (str): Output directory
+        format (str, optional): VTF format mode. Defaults to "dxt5".
+        pause (bool, optional): Whether to wait for conversion before continuing. Defaults to False.
+    """
+    search = indir + "\*.png"
+    args = [
+        "./VTFCmd.exe",
+        "-folder",
+        search,
+        "-output",
+        outdir,
+        "-format",
+        format,
+        "-silent",
+    ]
+
+    sp = subprocess.Popen(args)
+    if pause:
+        sp.wait()
