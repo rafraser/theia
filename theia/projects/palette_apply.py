6d972809efd92c488d6e81d400bbc756d0476ad8
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-12-11 00:17:21 +1000

    Allow palette name overrides

--- theia/projects/palette_apply.py
+++ theia/projects/palette_apply.py
@@ -10,11 +10,17 @@ def main(args):
     path = args.output + "/" + args.palette + "/"
     os.makedirs(path, exist_ok=True)
 
+    if args.names:
+        if len(args.names) != len(colors):
+            print("Number of names given must match number of colors in palette!")
+            exit(1)
+
     for image in args.images:
         im = Image.open(f"{args.input}/{image}.png").convert("RGBA")
         for n, color in enumerate(colors):
             im_colorized = multiply(im, color)
-            im_colorized.save(f"{path}{image}_{str(n)}.png")
+            suffix = args.names[n] if args.names else str(n + 1)
+            im_colorized.save(f"{path}{image}_{suffix}.png")
 
 
 if __name__ == "__main__":
@@ -22,6 +28,7 @@ if __name__ == "__main__":
     parser.add_argument("palette")
     parser.add_argument("--input", default="input")
     parser.add_argument("--output", default="output")
-    parser.add_argument("-i", "--images", nargs="+", required=True)
+    parser.add_argument("--images", nargs="+", required=True)
+    parser.add_argument("--names", nargs="+")
     args = parser.parse_args()
     main(args)
