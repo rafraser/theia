2cb81fb1627358624455b06fa891fe440c805641
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-03-07 17:06:24 +1000

    Project setup - part one

--- theia/__init__.py
+++ theia/__init__.py
@@ -1,1 +1,8 @@
-
+import theia.channels as channels
+import theia.color as color
+import theia.flaticon as flaticon
+import theia.gif as gif
+import theia.grid as grid
+import theia.image as image
+import theia.outline as outline
+import theia.palettes as palettes
